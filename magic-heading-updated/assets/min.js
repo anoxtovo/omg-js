e02353265f14630880a351c9a7ae222d481c4fc0
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2021-10-24 16:51:15 +0530

    Updated: assets/main.css; assets/min.js | magic-heading-updated

--- magic-heading-updated/assets/min.js
+++ magic-heading-updated/assets/min.js
@@ -4,13 +4,29 @@ var magicInput = window.prompt("What is the MAGIC keyword to Open door");
 // magic keyword
 var magicVariable = "bob";
 
+function reloadGame(){
+	//var linkSelect = document.querySelector('#reset');
+
+	var linkSelect = document.getElementById('reset');
+
+	linkSelect.addEventListener('click', function(){
+
+		// reload function
+		location.reload();
+	});
+}
+
 if (magicVariable === magicInput) {
-	document.write("<h1> Congratulations...</h1><span>Hey "+magicVariable+"! Here your,</span><br></h1>");
+	document.write("<h1 class='head-one'> Congratulations...</h1><span>Hey "+magicVariable+"! Here your,</span><br></h1>");
 				
-	document.write("<br><a href='#'>Magic Link </a>");
+	document.write("<br><a class='link-one' href='#' id='magicnum'>Magic Link </a>");
+
+	// refresh link
+	document.write("<a href='#' id='reset'>Reload</a>");
+	reloadGame();
 
 	// select link and change HEADING
-	var linkSelect = document.querySelector("a");
+	var linkSelect = document.querySelector("magicnum");
 
 	linkSelect.addEventListener('click', function(){
 					// select & change h1 content
@@ -23,13 +39,14 @@ if (magicVariable === magicInput) {
 	console.log("Hello master "+magicVariable+", Oops -_- , Oh Memory your not master "+magicVariable+".");
 
 	// refresh link
-	document.write("<br><a href='#'>Reload</a>");
+	document.write("<br><a href='#' id='reset'>Reload</a>");
 
 	// select link and refresh function
-	var linkSelect = document.querySelector('a');
+	/*var linkSelect = document.querySelector('#reset');
 	linkSelect.addEventListener('click', function(){
 
 		// reload function
 		location.reload();
-	});
+	});*/
+	reloadGame();
 }
